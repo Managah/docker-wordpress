c0a75485eb2cf266a459222f6797885d5ebfdedf
Thuan Nguyen <me@ndthuan.com>
2019-11-08 09:15:29 +0700

    Increase default prefork servers

--- nuphp-entrypoint.sh
+++ nuphp-entrypoint.sh
@@ -2,9 +2,9 @@
 
 cat > /etc/apache2/mods-available/mpm_prefork.conf <<EOF
 <IfModule mpm_prefork_module>
-    StartServers              ${PREFORK_START_SERVERS:-1}
-    MinSpareServers           ${PREFORK_MIN_SPARE_SERVERS:-1}
-    MaxSpareServers           ${PREFORK_MAX_SPARE_SERVERS:-3}
+    StartServers              ${PREFORK_START_SERVERS:-3}
+    MinSpareServers           ${PREFORK_MIN_SPARE_SERVERS:-3}
+    MaxSpareServers           ${PREFORK_MAX_SPARE_SERVERS:-10}
     MaxRequestWorkers         ${PREFORK_MAX_REQUEST_WORKERS:-150}
     MaxConnectionsPerChild    ${PREFORK_MAX_CONNECTIONS_PER_CHILD:-0}
 </IfModule>
