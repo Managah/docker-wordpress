34b20583b154119d92ece509f04b53805fef0ab7
Thuan Nguyen <me@ndthuan.com>
2021-02-25 16:22:40 +0700

    Docker push after all images pass test

--- build.sh
+++ build.sh
@@ -2,14 +2,17 @@
 
 set -ex
 
-VERSIONS='php7.3-apache php7.4-apache'
+TAGS='php7.3-apache php7.4-apache'
 
-for tag in $VERSIONS; do
-    docker pull wordpress:$tag
+for tag in $TAGS; do
+    docker pull wordpress:${tag}
     docker build -t managah/wordpress:${tag} --build-arg BASE_TAG=${tag} -f apache2.Dockerfile .
-    docker run --name $tag --rm -d -p8888:80 managah/wordpress:${tag}
+    docker run --name ${tag} --rm -d -p8888:80 managah/wordpress:${tag}
     sleep 2
-    curl -s -i 'http://localhost:8888' | grep -q 'setup-config.php' || (echo "$tag not working" && (docker rm -fv $tag || true) && exit 1)
-    docker rm -fv $tag
+    curl -s -i 'http://localhost:8888' | grep -q '302 Found' || (echo "$tag not started" && (docker rm -fv ${tag} || true) && exit 1)
+    docker rm -fv ${tag}
+done
+
+for tag in $TAGS; do
     docker push managah/wordpress:${tag}
 done
